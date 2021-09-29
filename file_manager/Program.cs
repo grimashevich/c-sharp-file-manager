c4e014dee6162571dc9aa31e99925ba16f2f48d9
grimashevich <k.grimashevich@gmail.com>
2021-09-29 18:32:32 +0900

    Update classes for GUI

--- file_manager/Program.cs
+++ file_manager/Program.cs
@@ -6,12 +6,12 @@ namespace file_manager
     {
         static void Main(string[] args)
         {
-            var l1 = new SymLine('+', 5, 10, 110);
-            var l2 = new SymLine('+', 50, 5, 20, LineType.Vertical);
-            l1.Draw();
-            l2.Draw();
-            Console.ReadKey();
-            l1.Clear();
+
+            for (int i = 0; i < 10; i++)
+            {
+                var sq = new SymRectangle(5 + i, 5 + i, 10, 10);
+                sq.Draw();
+            }
             Console.ReadKey();
             
         }
---
+++ file_manager/SymRectangle.cs
@@ -0,0 +1,46 @@
+﻿using System.Globalization;
+
+namespace file_manager
+{
+    public class SymRectangle
+    {
+        /*Прямоугольник, рисуется слева-направо, сверху-вниз от заданной координаты*/
+        
+        /*Углы*/
+        private Point ltCorner;
+        private Point rtCorner;
+        private Point rbCorner;
+        private Point lbCorner;
+        
+        /*Стенки*/
+        private SymLine topLine;
+        private SymLine bottomLine;
+        private SymLine leftLine;
+        private SymLine rightLine;
+
+        public SymRectangle(int x, int y, int width, int height)
+        {
+            ltCorner = new Point(x, y, '╔');
+            rtCorner = new Point(x + width - 1, y, '╗');
+            rbCorner = new Point(x + width - 1, y + height - 1, '╝');
+            lbCorner = new Point(x, y + height - 1, '╚');
+
+            topLine = new SymLine('═', x + 1, y, width - 2, LineType.Horizontal);
+            bottomLine = new SymLine('═', x + 1, y + height - 1, width - 2, LineType.Horizontal);
+            leftLine = new SymLine('║', x, y + 1, height - 2, LineType.Vertical);
+            rightLine = new SymLine('║', x + width - 1, y + 1, height - 2, LineType.Vertical);
+        }
+
+        public void Draw()
+        {
+            ltCorner.Draw();
+            rtCorner.Draw();
+            rbCorner.Draw();
+            lbCorner.Draw();
+            topLine.Draw();
+            bottomLine.Draw();
+            leftLine.Draw();
+            rightLine.Draw();
+        }
+    }
+}
